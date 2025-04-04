b318cd7281e23057d49c9274f17bf9977b7b6dd1
andreii467 <andreiglomnicu9@gmail.com>
2025-04-04 18:44:04 +0300

    imported link library

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="home-page">
       <h1>Birds and Insects</h1>
       <div className="buttons">
-        <button className="insects-button">Insects</button>
+        <Link>
+          <button className="insects-button">Insects</button>
+        </Link>
         <button className="birds-buttons">Birds</button>
       </div>
     </div>
